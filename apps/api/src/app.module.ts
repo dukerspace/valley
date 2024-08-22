9264d000a5a0d1afe958f43e836c81e5d2ae035b
dukerspace <montolsaklor@gmail.com>
2024-08-22 15:21:20 +0700

    fix(api):  config main file

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,10 +1,19 @@
-import { Module } from '@nestjs/common';
-import { AdminModule } from './admin/admin.module';
-import { UserModule } from './user/user.module';
+import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+import { join } from 'path'
+import { AdminModule } from './admin/admin.module'
+import { UserModule } from './user/user.module'
 
 @Module({
-  imports: [AdminModule, UserModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: join(__dirname, '../../../.env'),
+      isGlobal: true
+    }),
+    AdminModule,
+    UserModule
+  ],
   controllers: [],
-  providers: [],
+  providers: []
 })
 export class AppModule {}
--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core'
+import { json, urlencoded } from 'express'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
-  await app.listen(3001)
+  const app = await NestFactory.create(AppModule, {
+    cors: true
+  })
+  app.use(json({ limit: '50mb' }))
+  app.use(urlencoded({ extended: true, limit: '50mb' }))
+  app.setGlobalPrefix(process.env.API_PREFIX)
+  app.enableShutdownHooks()
+  await app.listen(process.env.API_PORT)
+  console.log('start', process.env.API_PORT, process.env.API_PREFIX)
 }
 bootstrap()
