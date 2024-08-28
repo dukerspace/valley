bf00c6ed42b89cdcab19f04ad87df6b09b7c3608
dukerspace <montolsaklor@gmail.com>
2024-08-28 22:48:40 +0700

    fix: http exeption filter & validation pipe

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,11 +1,27 @@
+import { BadRequestException, ValidationPipe } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
+import { IErrorMessage } from 'dukerspace/utils'
 import { json, urlencoded } from 'express'
 import { AppModule } from './app.module'
+import { HttpExceptionFilter } from './http-exception'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     cors: true
   })
+  app.useGlobalFilters(new HttpExceptionFilter())
+  app.useGlobalPipes(
+    new ValidationPipe({
+      exceptionFactory: (errors) => {
+        const result: IErrorMessage[] = errors.map((error) => ({
+          property: error.property,
+          message: error.constraints[Object.keys(error.constraints)[0]]
+        }))
+        return new BadRequestException(result)
+      },
+      stopAtFirstError: true
+    })
+  )
   app.use(json({ limit: '50mb' }))
   app.use(urlencoded({ extended: true, limit: '50mb' }))
   app.setGlobalPrefix(process.env.API_PREFIX)
