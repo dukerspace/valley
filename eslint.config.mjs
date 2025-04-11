59722a17b42b82e858f7fdb3fb35f345887682c3
dukerspace <montolsaklor@gmail.com>
2025-04-11 13:09:14 +0700

    fix: eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,17 +1,32 @@
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-
+import { default as pluginJs } from '@eslint/js';
+import tseslint from 'typescript-eslint';
 
 /** @type {import('eslint').Linter.Config[]} */
+
+
 export default [
+  {
+    ignores: [
+      '**/.*.js',
+      '**/*.setup.js',
+      '**/*.config.js',
+      '**/.turbo/',
+      '**/dist/',
+      '**/coverage/',
+      '**/node_modules/',
+      'eslint.config.mjs',
+      '**/*.json',
+      '**/*.next',
+    ],
+  },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   {
     rules: {
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-unsafe-argument': 'off',
-      "no-unused-vars": "off",
-      "@typescript-eslint/no-unused-vars": "off"
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': 'off'
     }
   }
 ];
